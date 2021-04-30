08ed997406e98488299f5ff637a8f748e19d2275
Genji-MS <Genji.Tapia@students.makeschool.com>
2021-04-29 20:14:55 -0700

    user can leave and list will update, but not for other users?

--- public/index.js
+++ public/index.js
@@ -4,6 +4,8 @@ $(document).ready( () => {
 
   //Keep track of the current user
   let currentUser;
+  // Get the online users from the server
+  socket.emit('get online users');
 
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
@@ -18,13 +20,6 @@ $(document).ready( () => {
     }
   });
 
-    //socket listeners
-  socket.on('new user', (username) => {
-    console.log(`${username} has joined the chat`);
-    // Add the new user to the online users div
-    $('.users-online').append(`<div class="user-online">${username}</div>`);
-  })
-
   $('#send-chat-btn').click((e)=>{
     e.preventDefault();
     if($('#chat-input').val().length > 0){
@@ -38,6 +33,14 @@ $(document).ready( () => {
     }
   });
 
+  //socket listeners
+
+  socket.on('new user', (username) => {
+    console.log(`${username} has joined the chat`);
+    // Add the new user to the online users div
+    $('.users-online').append(`<div class="user-online">${username}</div>`);
+  })
+
   socket.on('new msg', (data) => {
     $('.message-container').append(`
     <div class="message">
@@ -45,6 +48,22 @@ $(document).ready( () => {
       <p class="message-text">${data.message}</p>
     </div>
     `);
+  })
+
+  socket.on('get online users', (onlineUsers) => {
+    //You may have not have seen this for loop before. It's syntax is for(key in obj)
+    //Our usernames are keys in the object of onlineUsers.
+    for(username in onlineUsers){
+      $('.users-online').append(`<div class="user-online">${username}</div>`);
+    }
+  })
+
+  socket.on('user has left', (onlineUsers) => {
+    console.log(`user has left the chat`);
+    $('.users-online').empty();
+    for(username in onlineUsers){
+      $('.users-online').append(`<div class="user-online">${username}</div>`);
+    }
   })
 
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,20 +1,36 @@
 //chat.js
-module.exports = (io, socket) => {
+module.exports = (io, socket, onlineUsers) => {
     // Listen for "new user" socket emits
 
     //io.emit sends data to all clients on the connection.
     //socket.emit sends data to the client that sent the original data to the server.
     socket.on('new user', (username) => {
-        console.log(`✋ ${username} has joined the chat! ✋`);
+        //Save the username as key to access the user's socket id
+        onlineUsers[username] = socket.id;
+        //Save the username to socket as well. This is important for later.
+        socket["username"] = username;
         //Send the username to all clients currently connected
+        console.log(`✋ ${username} has joined the chat! ✋`);
         io.emit("new user", username);
     })
 
-
+    //triggers index.js socket.emit('new msg')
     socket.on('new msg', (data) => {
         // Send that data back to ALL clients
         console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
         io.emit('new msg', data);
     })
   
+    socket.on('get online users', () => {
+        //Send over the onlineUsers
+        socket.emit('get online users', onlineUsers);
+    })
+
+    // This fires when a user closes out of the application
+    // socket.on("disconnect") is a special listener that fires when a user exits out of the application.
+    socket.on('disconnect', () => {
+        //This deletes the user by using the username we saved to the socket
+        delete onlineUsers[socket.username];
+        socket.emit('user has left', onlineUsers);
+    })
 }
